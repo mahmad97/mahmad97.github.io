67e3d1ff4985a3709097bb745594507923fec182
Mohammad Ahmad <mahmad97taha@gmail.com>
2025-08-08 00:01:17 -0500

    add fonts

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,15 @@ import './App.css';
 const App = () => {
 	return (
 		<>
-			<p>Hello Vite</p>
-			<p>This is Mohammad's website</p>
+			<div>
+				<p>Hello Vite</p>
+				<p>This is Mohammad's website</p>
+			</div>
+			<div>
+				<p className='font-sans'>This is Inter.</p>
+				<p className='font-mono'>This text is in Roboto.</p>
+				<p className='font-serif'>And this text is in fancy Garamond.</p>
+			</div>
 		</>
 	);
 };
