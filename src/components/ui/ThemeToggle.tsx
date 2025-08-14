e9675449b11154101129c9b6bd9f9104cfb069ac
Mohammad Ahmad <mahmad97taha@gmail.com>
2025-08-14 13:21:32 -0500

    replace toggle text with icons

--- src/components/ui/ThemeToggle.tsx
+++ src/components/ui/ThemeToggle.tsx
@@ -1,3 +1,5 @@
+import { Sun, Moon, Monitor } from 'lucide-react';
+
 import { useTheme } from '../../hooks/useTheme';
 
 const ThemeToggle = () => {
@@ -13,10 +15,21 @@ const ThemeToggle = () => {
 		}
 	};
 
-	const buttonText =
-		theme === 'light' ? 'Light' : theme === 'dark' ? 'Dark' : 'System';
-
-	return <button onClick={handleClick}>{buttonText}</button>;
+	const icon =
+		theme === 'light' ? (
+			<Sun className='w-5 h-5' strokeWidth={2.5} />
+		) : theme === 'dark' ? (
+			<Moon className='w-5 h-5' strokeWidth={2.5} />
+		) : (
+			<Monitor className='w-5 h-5' strokeWidth={2.5} />
+		);
+	return (
+		<button
+			onClick={handleClick}
+			className='w-8 h-8 border flex items-center justify-center rounded-md border-slate-400 dark:border-slate-600 bg-gray-100 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700 text-blue-500 transition-colors'>
+			{icon}
+		</button>
+	);
 };
 
 export default ThemeToggle;
