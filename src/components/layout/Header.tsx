623d9f623213dc12968dafd37e8fa895e7c5141c
Mohammad Ahmad <mahmad97taha@gmail.com>
2025-08-14 13:21:10 -0500

    update styles

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,34 +3,33 @@ import { Link } from '@tanstack/react-router';
 import ThemeToggle from '../ui/ThemeToggle';
 
 const Header = () => {
+	const navTextStyle =
+		'text-base font-normal [&.active]:font-bold text-gray-800 dark:text-gray-200';
+
 	return (
-		<header className='flex h-14 px-8 border-b items-center justify-between'>
+		<header className='flex h-14 px-8 border-b items-center justify-between border-slate-400 dark:border-slate-600 backdrop-blur'>
 			<Link to='/' className='flex gap-4'>
 				<div className=''>👤</div>
 				<p className='text-xl'>Mohammad Ahmad</p>
 			</Link>
 
-			<div className='flex gap-4'>
+			<div className='flex gap-4 items-center'>
 				<div>Search</div>
 
-				<div className='border-l'></div>
-
-				<nav className='flex gap-4'>
-					<Link to='/' className='[&.active]:font-bold'>
+				<nav className='px-4 border-x flex gap-4 border-slate-400 dark:border-slate-600'>
+					<Link to='/' className={navTextStyle}>
 						Home
 					</Link>
-					<Link to='/about' className='[&.active]:font-bold'>
+					<Link to='/about' className={navTextStyle}>
 						About
 					</Link>
-					<Link to='/research' className='[&.active]:font-bold'>
+					<Link to='/research' className={navTextStyle}>
 						Research
 					</Link>
 					{/* <Link to='/blog' className='[&.active]:font-bold'>
 						Blog
 					</Link> */}
 				</nav>
-
-				<div className='border-l'></div>
 
 				<ThemeToggle />
 			</div>
