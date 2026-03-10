8f9901e1a77f8c2a35c7e283dfa017588afe25b7
Mohammad Ahmad <mahmad97taha@gmail.com>
2026-03-10 13:22:49 -0500

    add projects page

---
+++ src/pages/Projects.tsx
@@ -0,0 +1,11 @@
+import type { ReactElement } from 'react';
+
+import PageContainer from '@/components/layout/PageContainer';
+
+const Projects = (): ReactElement => (
+	<PageContainer heading='Projects'>
+		<p>Work in progress...</p>
+	</PageContainer>
+);
+
+export default Projects;
