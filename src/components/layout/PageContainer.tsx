8eaaf1d1b2c4ae44744a6a723cd1034e1d5e77d3
Mohammad Ahmad <mahmad97taha@gmail.com>
2026-03-18 15:16:41 -0500

    add title modification on each page

--- src/components/layout/PageContainer.tsx
+++ src/components/layout/PageContainer.tsx
@@ -1,6 +1,10 @@
 import type { ReactElement, ReactNode } from 'react';
 
+import { useEffect } from 'react';
+
 import { Heading } from '@/components/typography';
+
+const BASE_TITLE = 'Mohammad Ahmad';
 
 type PageContainerProps = Readonly<{
 	children: ReactNode;
@@ -11,6 +15,13 @@ const PageContainer = ({
 	children,
 	heading,
 }: PageContainerProps): ReactElement => {
+	useEffect(() => {
+		document.title = heading ? `${BASE_TITLE} | ${heading}` : BASE_TITLE;
+		return () => {
+			document.title = BASE_TITLE;
+		};
+	}, [heading]);
+
 	return (
 		<div className='max-w-5xl mx-auto p-4 md:p-8 flex flex-col gap-8'>
 			{heading && <Heading>{heading}</Heading>}
